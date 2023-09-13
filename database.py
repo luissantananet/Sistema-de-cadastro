4d922caa54f55970b4a3e44548ceb31158ef321a
luissantananet <luissantananet@gmail.com>
2023-09-13 16:40:00 -0300

    update

--- database.py
+++ database.py
@@ -1,3 +1,4 @@
+# Criação de bando de dados(criar arquivo separando "database")
 import sqlite3
 
 class Data_base:
@@ -18,7 +19,6 @@ class Data_base:
         cursor = self.connection.cursor()
         cursor.execute("""
             CREATE TABLE IF NOT EXISTS Empresas(
-
             CNPJ TEXT,
             NOME TEXT,
             LOGRADOURO TEXT,
@@ -30,29 +30,19 @@ class Data_base:
             CEP TEXT,
             TELEFONE TEXT,
             EMAIL TEXT,
-
             PRIMARY KEY(CNPJ)
-            );
+            );""")
 
-           
-        
-        
-        """)
-    
     def register_company(self, fullDataSet):
-
         campos_tabela = ('CNPJ','NOME','LOGRADOURO','NUMERO','COMPLEMENTO','BAIRRO','MUNICIPIO',
         'UF','CEP','TELEFONE','EMAIL')
-
         qntd = ("?,?,?,?,?,?,?,?,?,?,?")
         cursor = self.connection.cursor()
-
         try:
             cursor.execute(f"""INSERT INTO Empresas {campos_tabela}
             VALUES({qntd})""", fullDataSet)
             self.connection.commit()
             return("OK")
-
         except:
             return "Erro"
 
@@ -66,23 +56,17 @@ class Data_base:
             pass
 
     def delete_companies(self, id):
-
         try:
             cursor = self.connection.cursor()
             cursor.execute(f"DELETE FROM Empresas WHERE CNPJ = '{id}' ")
-
             self.connection.commit()
-
             return "Cadastro de empresa excluido com sucesso!"
-
         except:
             return "Erro ao Excluir registro!"
 
     def update_company(self, fullDataSet):
-
         cursor = self.connection.cursor()
         cursor.execute(f""" UPDATE Empresas set
-
             CNPJ = '{fullDataSet[0]}',
             NOME = '{fullDataSet[1]}',
             LOGRADOURO = '{fullDataSet[2]}',
@@ -94,7 +78,5 @@ class Data_base:
             CEP = '{fullDataSet[8]}',
             TELEFONE = '{fullDataSet[9]}',
             EMAIL = '{fullDataSet[10]}'
-
             WHERE CNPJ = '{fullDataSet[0]}'""")
-
         self.connection.commit()
